d6b3e9116224b1e7e5f303b9be4c1872c952bdec
yakuhito <y@kuhi.to>
2022-01-05 14:24:42 +0200

    update job.sh

--- job.sh
+++ job.sh
@@ -3,7 +3,9 @@
 . /chia-blockchain/venv/bin/activate
 chia stop all -d
 cd /root/.chia/mainnet/db/
-zip -9 -r blockchain_v1_mainnet.sqlite.zip blockchain_v1_mainnet.sqlite
-mv blockchain_v1_mainnet.sqlite.zip /var/www/html
+zip -9 -r blockchain_v1_mainnet.sqlite.zip.2 blockchain_v1_mainnet.sqlite
+mv blockchain_v1_mainnet.sqlite.zip.2 /var/www/html
+cd /var/www/html
+mv blockchain_v1_mainnet.sqlite.zip.2 blockchain_v1_mainnet.sqlite.zip
 chown -R www-data:www-data /var/www/html
 chia start node
