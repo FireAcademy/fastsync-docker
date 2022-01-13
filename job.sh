59fdb246e4566dc54a3f5afc5c208bede7a17f2e
yakuhito <y@kuhi.to>
2022-01-13 13:54:09 +0200

    whoops

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 . /chia-blockchain/venv/bin/activate
 chia stop all -d
 cd /root/.chia/mainnet/db/
-mv blockchain_v1_mainnet.sqlite /var/www/html/blockchain_v1_mainnet.sqlite.2
+cp blockchain_v1_mainnet.sqlite /var/www/html/blockchain_v1_mainnet.sqlite.2
 cd /var/www/html
 mv blockchain_v1_mainnet.sqlite.2 blockchain_v1_mainnet.sqlite
 chown -R www-data:www-data /var/www/html
