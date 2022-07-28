ca3707909e80c1e0e9ef7e70de3b52191824ed9a
yakuhito <y@kuhi.to>
2022-07-28 12:38:39 +0300

    back job: use sqlite3 vacuum and don't stop node

--- job.sh
+++ job.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 . /chia-blockchain/venv/bin/activate
-chia stop all
 
 cd /root/.chia/mainnet/db/
-test -f blockchain_v2_mainnet.sqlite && cp blockchain_v2_mainnet.sqlite /var/www/html/blockchain_v2_mainnet.sqlite.2
-test -f blockchain_v2_testnet10.sqlite && cp blockchain_v2_testnet10.sqlite /var/www/html/blockchain_v2_testnet10.sqlite.2
+test -f blockchain_v2_mainnet.sqlite && sqlite3 ./blockchain_v2_mainnet.sqlite "vacuum into '/var/www/html/blockchain_v2_mainnet.sqlite.bak'"
+test -f blockchain_v2_testnet10.sqlite && sqlite3 ./blockchain_v2_testnet10.sqlite "vacuum into '/var/www/html/blockchain_v2_testnet10.sqlite.bak'"
 
 cd /var/www/html
-test -f blockchain_v2_mainnet.sqlite.2 && mv blockchain_v2_mainnet.sqlite.2 blockchain_v2_mainnet.sqlite
-test -f blockchain_v2_testnet10.sqlite.2 && mv blockchain_v2_testnet10.sqlite.2 blockchain_v2_testnet10.sqlite
+test -f blockchain_v2_mainnet.sqlite.bak && mv blockchain_v2_mainnet.sqlite.2 blockchain_v2_mainnet.sqlite
+test -f blockchain_v2_testnet10.sqlite.bak && mv blockchain_v2_testnet10.sqlite.2 blockchain_v2_testnet10.sqlite
 
 chown -R www-data:www-data /var/www/html
-chia start node
