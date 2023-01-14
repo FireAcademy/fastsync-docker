a026d9589958cf978343d138d069659733300138
yakuhito <y@kuhi.to>
2023-01-13 23:50:17 -0800

    fix it

--- job.sh
+++ job.sh
@@ -6,11 +6,13 @@ cd /root/.chia/mainnet/db/
 test -f blockchain_v2_mainnet.sqlite && sqlite3 ./blockchain_v2_mainnet.sqlite "vacuum into '/var/www/html/blockchain_v2_mainnet.sqlite.bak'"
 test -f blockchain_v2_testnet10.sqlite && sqlite3 ./blockchain_v2_testnet10.sqlite "vacuum into '/var/www/html/blockchain_v2_testnet10.sqlite.bak'"
 test -f height-to-hash && cp height-to-hash /var/www/html/height-to-hash.bak
+test -f sub-epoch-summaries && cp sub-epoch-summaries /var/www/html/sub-epoch-summaries.bak
 
 cd /var/www/html
 chown www-data:www-data *
 test -f blockchain_v2_mainnet.sqlite.bak && mv blockchain_v2_mainnet.sqlite.bak blockchain_v2_mainnet.sqlite
 test -f blockchain_v2_testnet10.sqlite.bak && mv blockchain_v2_testnet10.sqlite.bak blockchain_v2_testnet10.sqlite
 test -f height-to-hash.bak && mv height-to-hash.bak height-to-hash
+test -f sub-epoch-summaries.bak && mv sub-epoch-summaries.bak height-to-hash
 
 chown -R www-data:www-data /var/www/html
--- start.sh
+++ start.sh
@@ -4,11 +4,13 @@ chia stop all
 # start web server
 touch /var/www/html/blockchain_v2_mainnet.sqlite
 touch /var/www/html/height-to-hash
+touch /var/www/html/sub-epoch-summaries
 service apache2 start
 
 # sync
 mkdir -p "$CHIA_ROOT/db"
 test -f /var/www/html/height-to-hash && cp /var/www/html/height-to-hash "$CHIA_ROOT/db/"
+test -f /var/www/html/sub-epoch-summaries && cp /var/www/html/sub-epoch-summaries "$CHIA_ROOT/db/"
 test -f /var/www/html/blockchain_v2_mainnet.sqlite && cp /var/www/html/blockchain_v2_mainnet.sqlite "$CHIA_ROOT/db/"
 test -f /var/www/html/blockchain_v2_testnet10.sqlite && cp /var/www/html/blockchain_v2_testnet10.sqlite "$CHIA_ROOT/db/"
 
